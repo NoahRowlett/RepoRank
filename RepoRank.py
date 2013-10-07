2133a1dec6fdabde5ccac6b1e99a7552b1be3c58
Woofles <salty_cea_dog@hotmail.com>
2013-10-07 00:52:26 -0500

    quick fix main

--- RepoRank.py
+++ RepoRank.py
@@ -5,7 +5,7 @@ import requests
 import getpass as g
 from repoClass import *
 
-def __main__(): 
+def main(): 
   url = 'https://api.github.com/repositories'
   username = raw_input("Enter username: ")
   password = g.getpass()
@@ -18,12 +18,11 @@ def __main__():
     tempurl = url
     for i in range(0, len(info) - 1):
       temp = info[i]
-      # tempurl[0:-7] means taking 'https://api.github.com/repositories' and making it 'https://api.github.com/repos'
-      url = tempurl[0:-7] + '/' + temp['full_name']
+      url = 'https://api.github.com/repos/' + temp['full_name']
       r = requests.get(url, auth=(username,password))
       stuff = json.loads(r.content)
       repos.append(Repo(stuff['name'], watchers=stuff['watchers_count'], issues=stuff['open_issues_count']))
       print repos[-1].get_name() + " " + str(repos[-1].get_watchers()) + " " + str(repos[-1].get_issues())
     url = link[link.find('<')+1:link.find('>')]
 
-__main__()
+if  __name__ =='__main__':main()
