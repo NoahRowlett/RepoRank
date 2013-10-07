5a84a9cdbf642447607ff0713dab37d751b3296b
Woofles <salty_cea_dog@hotmail.com>
2013-10-06 22:31:57 -0500

    noah sux

--- RepoRank.py
+++ RepoRank.py
@@ -18,12 +18,18 @@ def __main__():
     html = response.read()
     headers = response.info()
     link = headers['Link']
-    url = link[link.find('<')+1:link.find('>')]
     info = json.loads(html)
     for i in range(0, len(info) - 1):
-      temp = info[-1]
-      repos.append(Repo(temp['name'], watchers=temp['watchers']))
-      print repos[-1].get_name()
-      print repos[-1].get_watchers()
-      
+      temp = info[i]
+      # repos.append(Repo(temp['name'], watchers=temp['watchers_count']))
+      url = url[0:-7] + '/' + temp['full_name']
+      print url
+      request = urllib.Request(url)
+      base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+      request.add_header("Authorization", "Basic %s" % base64string)  
+      response = urllib.urlopen(url)
+      print response[0]['name']
+      # print repos[-1].get_name()
+      # print repos[-1].get_watchers()
+    url = link[link.find('<')+1:link.find('>')]
 __main__()
