fe699e2fe5199842e25cbc984940f494fda3cd31
Woofles <salty_cea_dog@hotmail.com>
2013-10-06 21:45:34 -0500

    hi noah

--- RepoRank.py
+++ RepoRank.py
@@ -1,14 +1,23 @@
 import urllib2 as urllib
 import json
+import base64
+import getpass as g
 from repo_class import *
 
 def __main__(): 
+  url = 'https://api.github.com/repositories'
+  username = 'Woofles'
+  password = g.getpass()
   for page in range(1, 11):
-    url = 'https://api.github.com/repositories?page=' + str(page)
-    print url
     repos = []
+    request = urllib.Request(url)
+    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+    request.add_header("Authorization", "Basic %s" % base64string)  
     response = urllib.urlopen(url)
     html = response.read()
+    headers = response.info()
+    link = headers['Link']
+    url = link[link.find('<')+1:link.find('>')]
     info = json.loads(html)
     for i in range(0, len(info) - 1):
       temp = info[i]
@@ -16,5 +25,3 @@ def __main__():
       print repos[i].get_name()
 
 __main__()
-
-#test change
